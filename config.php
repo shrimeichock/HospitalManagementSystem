11370ee5feb7741e735bb251d4cb6417e69a659b
Shrimei Chock <meichock1@gmail.com>
2022-05-10 18:28:29 -0400

    connect to heroku myPHPAdmin database

--- config.php
+++ config.php
@@ -1,10 +1,21 @@
 <?php
 
-try{
+/*try{
     $db = new PDO('sqlite:hospital_management.db');
     
 }catch(PDOException $e){
     echo $e->getMessage();
-}
+}*/
+
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
 ?>
