3d05f243d59680824fd1ce6ebc4d813a390a9de0
Shrimei Chock <meichock1@gmail.com>
2022-05-10 21:55:16 -0400

    add debug statement for id

--- remove.php
+++ remove.php
@@ -6,6 +6,7 @@ $string = $_GET['remove'];
 
 if(substr($string, 0,13)=='removePatient'){
     $id = substr($string, 13);
+    json_encode($id);
     $sql = "DELETE FROM patients WHERE ID={$id}";
     /*$sql2 = "DELETE FROM is_experiencing WHERE Patient_id = {$id}";
     $sql3 = "DELETE FROM sick_from WHERE Patient_id = {$id}";
