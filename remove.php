4f2153144e59c28ca1b15333a94f36e701e0eda8
Shrimei Chock <meichock1@gmail.com>
2022-05-10 22:15:30 -0400

    add query for remove

--- remove.php
+++ remove.php
@@ -6,15 +6,15 @@ $string = $_GET['remove'];
 
 if(substr($string, 0,13)=='removePatient'){
     $id = substr($string, 13);
-    echo "{$id}";
+    //echo "{$id}";
     $sql = "DELETE FROM patients WHERE ID={$id}";
     /*$sql2 = "DELETE FROM is_experiencing WHERE Patient_id = {$id}";
     $sql3 = "DELETE FROM sick_from WHERE Patient_id = {$id}";
     $sql4 = "DELETE FROM assigned_to WHERE Patient_id = {$id}";
-    $result = mysqli_query($connect, $sql);
     $result2 = mysqli_query($connect, $sql2);
     $result3 = mysqli_query($connect, $sql3);
     $result4 = mysqli_query($connect, $sql4);*/
+    $result = mysqli_query($connect, $sql);
     //echo $sql;
 }elseif(substr($string, 0,12)=='removeDoctor'){
     $id = substr($string, 12);
