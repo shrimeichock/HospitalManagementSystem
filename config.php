d85686792f87edad625251330eb626805ebeee0f
Mei Chock <59775096+shrimeichock@users.noreply.github.com>
2022-05-29 10:19:05 -0400

    Switch back to XAMPP
    Remove ClearDB

--- config.php
+++ config.php
@@ -1,13 +1,11 @@
 <?php
-
-//Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
-$active_group = 'default';
-$query_builder = TRUE;
-// Connect to DB
-$connect = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+    $server = "localhost";
+    $username = "hospitalAdmin";
+    $password = "hospitalAdmin*123";
+    $database = "hospital";
+    
+    $connect = mysqli_connect($server, $username, $password, $database);
+    if(!$connect){
+        die("Connection failed: " . mysqli_connect_error());
+    }
 ?>
